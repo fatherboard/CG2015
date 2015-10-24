15d15a6daf393e03896caac5cf3df7d561b5a827
fatherboard <pedro.bucho@gmail.com>
2015-10-24 13:28:59 +0100

    merge2

---
+++ proj/Obstacle.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef __OBSTACLE__
+#define __OBSTACLE__
+
+#include "DynamicObject.h"
+
+class Obstacle: public DynamicObject {
+	public:
+		Obstacle();
+		~Obstacle();
+};
+
+#endif
---
+++ proj/StaticObject.cpp
@@ -0,0 +1,18 @@
+/*
+ * StaticObject.cpp
+ *
+ *  Created on: 30/09/2015
+ *      Author: 5702pedro.bucho
+ */
+
+#include "StaticObject.h"
+
+StaticObject::StaticObject() {
+	// TODO Auto-generated constructor stub
+
+}
+
+StaticObject::~StaticObject() {
+	// TODO Auto-generated destructor stub
+}
+
